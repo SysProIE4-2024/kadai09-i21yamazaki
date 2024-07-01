9f316ea40ee6ac256761c0274f09dad36c13e2ef
i21yamazaki <i21yamazaki@gak.tokuyama.ac.jp>
2024-07-02 08:49:52 +0900

    myenvを実装, 動作テストを実施

--- myenv.c
+++ myenv.c
@@ -9,11 +9,50 @@
 #include "myputenv.h"
 #endif
 
-int main() {
+extern char **environ;
+
+int main(int argc, char *argv[]) {
+  char **it = argv + 1;
+  while (*it != NULL) {
+    if (putenv(*it) < 0) {
+      break;
+    }
+    ++it;
+  }
+  if (*it != NULL) {
+    execvp(*it, it);
+    return 1; // execvp が戻る -> エラー
+  }
+  for (char **env = environ; *env != NULL; ++env) {
+    puts(*env);
+  }
+  return 0;
 }
 
 /* 実行例
 
 ここに実行例を書く
+
+% ./myenv
+MallocNanoZone=0
+USER=sy
+...
+_=/Users/sy/Documents/syspro/kadai09-i21yamazaki/./myenv
+
+% ./myenv LC_TIME=C
+MallocNanoZone=0
+USER=sy
+...
+_=/Users/sy/Documents/syspro/kadai09-i21yamazaki/./myenv
+LC_TIME=C
+
+% ./myenv LC_TIME=C date
+Tue Jul  2 08:42:52 JST 2024
+
+% ./myenv LC_TIME=ja_JP.UTF-8 TZ=Cuba date
+2024年 7月 1日 月曜日 19時47分15秒 CDT
+
+% ./myenv echo hello
+hello
 
 */
